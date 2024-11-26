d2b0ab1a8078858ba796c82c2fe7546172281cf8
kontol66666 <akagaminojeki39@gmail.com>
2024-11-27 00:00:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+global.owner = "6283865233188" //owner number
+global.botname = "Developed By Tama Ryuichi"
+global.footer = "Tama Ryuichi" //footer section
+global.status = true //"self/public" section of the bot
+global.mess = {
+ingroup: "It's not funny, this feature is only for groups💢",
+admin: "not funny, only group admins use this feature💢",
+owner: "Wow! You're not my owner!",
+premium: "You are not a premium user",
+seller: "you don't have access as a seller yet?",
+wait: "please just wait bruh.."
+}
+
+global.packname = "Tama Ryuichi"
+global.author = "2009"
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
